5b65d4bacc865606185ff94543953777bcdef3e5
Islam412 <islam.124.hamdy@gmail.com>
2026-01-08 01:49:34 +0200

    desing usercustom admin

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from userauths.models import User, Profile , Phone , CreditCard , PasswordResetToken
 
-# Register your models here.
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_filter = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    filter_horizontal = ('user_permissions',)
